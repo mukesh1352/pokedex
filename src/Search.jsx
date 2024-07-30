434dfc8981760b94177c24ca39d7b7d36b809085
Mukesh <mukeshnarravula14@gmail.com>
2024-07-30 22:38:17 +0530

    Added the filter icon

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,9 +1,11 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 function Search()
 {
     return(
         <div className="search-container">
             <input type="text" placeholder="Search" className="search-box-pokemon"></input>
+            <FontAwesomeIcon icon={faFilter} className='hover-box'/>
         </div>
     );
 }
